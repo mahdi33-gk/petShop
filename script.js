687ec7bc4a6b37e2fd7d43d1f72b1501e79bfb47
mehedi <mehedi25122003@gmail.com>
2025-02-19 21:32:12 +0600

    fetcher

--- script.js
+++ script.js
@@ -23,12 +23,14 @@ const btnShower = (id) => {
 btnAdder();
 
 
+
 // display all pets
 const displayPets = () => {
     fetch('https://openapi.programming-hero.com/api/peddy/pets')
     .then(res => res.json())
-    .then(data =>console.log(data))
+    .then(data => console.log(data.pets))
 }
+displayPets()
 const showsPets = (id) => {
 
 }
