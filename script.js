5dda7e101eff19a61b076b42e71959d797923393
mehedi <mehedi25122003@gmail.com>
2025-02-19 18:07:59 +0600

    failed

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const btnAdder = () =>{
 const btnShower = (id) => {
     const btnContainer = document.getElementById('btn-container');
     for(let btn of id){
-        const div = document.createElement('div');
+        const div = document.createElement('a');
         div.innerHTML = `
-        <a class="inline-flex text-center gap-2 btn px-7 py-4">
-            <img class="w-10" src="${btn.category_icon}" alt="">
-            <span>${btn.category}</span>
+        <a class="inline-flex border-borderCl border-2 md:text-center gap-2 btn px-7 p-2 justify-center">
+            <img class="md:w-8 w-4 h-4 md:h-8" src="${btn.category_icon}" alt="">
+            <span class="font-semibold md:text-4 text-sm">${btn.category}</span>
 
         </a>
         `;
