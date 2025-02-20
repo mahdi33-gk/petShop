7dfb3e9253bf505c8fb40e6a5ab6b7c15d26234c
mehedi <mehedi25122003@gmail.com>
2025-02-20 22:35:18 +0600

    done

--- script.js
+++ script.js
@@ -25,8 +25,9 @@ btnAdder();
 const loadcategoryData = (category) => {
     fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => showsPets(data.data))
 }
+
 
 
 
